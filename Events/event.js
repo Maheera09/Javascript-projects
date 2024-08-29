5bf714458885dd9cfda8dd75b75421d3df133e63
Maheera09 <maheerasaqib089@gmail.com>
2024-08-29 13:17:42 +0500

    Events

---
+++ Events/event.js
@@ -0,0 +1,33 @@
+// document.getElementById('owl').onclick = function(){
+//     alert("owl clicked")
+// } provides less features, hence not a recommended aproach 
+
+// document.getElementById('owl').addEventListener('click', function(e){
+//     // console.log(e)
+// }, false)
+
+// document.getElementById('images').addEventListener('click', function(e){
+//     console.log('Clicked inside the ul');
+// }, false)
+
+// document.getElementById('owl').addEventListener('click', function(e){
+//     console.log('Clicked inside the owl');
+//     e.stopPropagation()
+// }, false)
+
+// document.getElementById('google').addEventListener('click',function(e){
+//         e.preventDefault();
+//         e.stopPropagation()
+//         console.log("google clicked");
+//     }, false)
+
+
+    document.querySelector('#images').addEventListener('click', function(e){
+    console.log(e.target.parentNode)
+    console.log(e.target.tagName)
+    if (e.target.tagName == 'IMG') {
+    let removeIt = e.target.parentNode
+    console.log(e.target.id);
+        removeIt.remove()
+    }
+    }, false)
